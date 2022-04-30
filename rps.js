bee27dd1ce42acd289872459c6924163231c19e8
leandrocruz <leandro.dinamo@gmail.com>
2022-04-30 03:26:22 -0300

    Create CSS sheet, functions and HTML elements
    Functions created:
      *displayScore
      *endResult
      *disableButton
    
    *Huge rework on the function play.
    *Added DOM manipulation to show the result of each round and the game's
    current score.
    *Function getName worked to integrate the code properly.
    *Rework on the function playRound.
    *CSS sheet created to stylize a basic layout.
    *Added elements on the HTML to emulate the Rock Paper Scissors from the
    computer. Needs more work.

--- rps.js
+++ rps.js
@@ -1,14 +1,12 @@
 function getName() {
-    const button = document.querySelector('#btn');
-    button.addEventListener('click', () => {
-        const name = document.getElementById('challenger').value;
-        if (name === '' || name === undefined) {
-            return 'Mysterious Person';
-        } else {
-            return name;
-        }
-         
-    })
+    let name = document.getElementById('challenger').value;
+    document.getElementById('btn').disabled = true;
+    if (name == '') {
+        name = 'Mysterious Person';
+        play(name);
+    } else {
+        play(name);
+    } 
 }
 
 //The function below makes the machine generate a random number and tranforms this number into a string. 0 becomes Rock, 1 becomes Paper and 2 becomes Scissors
@@ -23,83 +21,112 @@ function computerPlay() {
     }
 }
 
-function buttonPlay() {
-    const buttons = document.querySelectorAll('button');
+const container = document.querySelector('#container');
+
+function play(name) {
+    let playerScore = 0;
+    let computerScore = 0;
+    let score = document.createElement('p');
+    score.textContent = `${name} -- ${playerScore} x ${computerScore} -- The MACHINE`
+    container.append(score)
+    let roundResult = document.createElement('p');
+    roundResult.textContent = `Hey ${name}, let's win this thing!`
+    container.append(roundResult)
+    const buttons = document.querySelectorAll('.rps');
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            if (button.id == 1) {
-                return (playRound('Rock', computerPlay()));
-            } else if (button.id == 2) {
-                return (playRound('Paper', computerPlay()));
-            } else {
-                return (playRound('Scissors', computerPlay()));
+            if (endResult(playerScore, computerScore)) {
+                if (button.id == 0) {
+                    const playerChoice = 'Rock';
+                    const computerChoice = computerPlay();
+                    if (playRound(playerChoice, computerChoice) == 0) {
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `TIE - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    } else if (playRound(playerChoice, computerChoice) == 1) {
+                        ++playerScore
+                        displayScore(score, playerScore, computerScore, name)
+                        roundResult.textContent = `YOU WON - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    } else if (playRound(playerChoice, computerChoice) == 2) {
+                        ++computerScore
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `YOU LOSE - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    }
+                } else if (button.id == 1) {
+                    const playerChoice = 'Paper';
+                    const computerChoice = computerPlay();
+                    if (playRound(playerChoice, computerChoice) == 0) {
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `TIE - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    } else if (playRound(playerChoice, computerChoice) == 1) {
+                        ++playerScore
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `YOU WON - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    } else if (playRound(playerChoice, computerChoice) == 2) {
+                        ++computerScore
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `YOU LOSE - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    }
+                } else {
+                    const playerChoice = 'Scissors';
+                    const computerChoice = computerPlay();
+                    if (playRound(playerChoice, computerChoice) == 0) {
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `TIE - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    } else if (playRound(playerChoice, computerChoice) == 1) {
+                        ++playerScore
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `YOU WON - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    } else if (playRound(playerChoice, computerChoice) == 2) {
+                        ++computerScore
+                        displayScore(score, playerScore, computerScore, name);
+                        roundResult.textContent = `YOU LOSE - ${name} -- [[${playerChoice}]] x [[${computerChoice}]] -- The MACHINE`;
+                    }
+                }
             }
         })
     })
 }
 
+function displayScore(score, playerScore, computerScore, name) {
+    score.textContent = `${name} -- ${playerScore} x ${computerScore} -- The MACHINE`
+}
+
 
 //The function below put the guesses of the two challengers to collide and returns a number accordingly to the result fo the round. 0 for tie, 1 for victory of the player, 2 for the victory of the machine.
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock') {
-        if (computerSelection === 'Rock') {
-            return 0;
-        } else if (computerSelection === 'Scissors') {
-            return 1;
-        } else if (computerSelection === 'Paper') {
-            return 2;
-        }
-    }
-
-    if (playerSelection === 'Paper'){
-        if (computerSelection === 'Paper') {
-            return 0;
-        } else if (computerSelection === 'Rock') {
-            return 1;
-        } else if (computerSelection === 'Scissors') {
-            return 2;
-        }
-    }
-
-    if (playerSelection === 'Scissors'){
-        if (computerSelection === 'Scissors') {
-            return 0;
-        } else if (computerSelection === 'Paper') {
-            return 1;
-        } else if (computerSelection === 'Rock') {
-            return 2;
-        }
-    }
-}
-
-
-function game() {
-    const name = getName();
-    const playerCount = 0;
-    const computerCount = 0;
-    while (i < 5 || j < 5) {
-        if (buttonPlay() === 0) {
-            console.log(`That's a Tie! [${name}] ${playerSelection} -VS- ${computerSelection} [The MACHINE]`);
-        } else if (buttonPlay() === 1) {
-            console.log(`You WIN! [${name}] **${playerSelection}** -VS- ${computerSelection} [The MACHINE]`);
-            playerCount += 1;
-        } else if (buttonPlay() === 2) {
-            console.log(`You LOSE! [${name}] ${playerSelection} -VS- **${computerSelection}** [The MACHINE]`);
-            computerCount += 1;
-        }
-    }
-
-    
-
-    if (playerCount > computerCount) {
-        console.log('You made', playerCount, 'point(s).');
-        console.log('The MACHINE made', computerCount, 'point(s).');
-        console.log(`${name}, you WON the whole thing! Congratulations!`);
+    if (playerSelection === computerSelection) {
+        return 0;
+    } else if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')
+        ) {
+        return 1
     } else {
-        console.log('You made', playerCount, 'point(s).');
-        console.log('The MACHINE made', computerCount, 'point(s).');
-        console.log(`${name}, you LOST the whole thing! Better luck next time.`)
+        return 2
     }
 }
 
-game()
+
+function endResult(playerScore, computerScore) {
+    let endResult = document.createElement('h3');
+    let keepPlaying = true;
+    container.append(endResult);
+    if (playerScore == 5) {
+        endResult.textContent = 'YOU WON the match';
+        keepPlaying = false
+        disableButton();
+    } else if (computerScore == 5) {
+        endResult.textContent = 'You LOST the match';
+        keepPlaying = false
+        disableButton();
+    } 
+    return keepPlaying;
+}
+
+function disableButton() {
+    const buttons = document.querySelectorAll('.rps');
+    buttons.forEach((button) =>{
+        button.disabled = true;
+    })
+}
